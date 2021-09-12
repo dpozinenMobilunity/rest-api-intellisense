44c7962b8f3079ffa7ac49e670c4abcc61a47382
GitHub Action <action@github.com>
2021-09-12 06:46:17 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/dpozinenmobilunity/restapiintellisense/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dpozinenmobilunity.restapiintellisense.services
+
+import com.github.dpozinenmobilunity.restapiintellisense.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/dpozinenmobilunity/restapiintellisense/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dpozinenmobilunity/restapiintellisense/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.dpozinenmobilunity.restapiintellisense.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dpozinenmobilunity.restapiintellisense.MyBundle
 
 class MyProjectService(project: Project) {
 
